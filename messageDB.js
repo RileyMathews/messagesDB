720c6700d11b1111fd5a58ee790fc0eb98880168
Riley Mathews <rileymathews80@gmail.com>
2018-04-16 13:26:52 -0500

    created database for local storage

---
+++ messageDB.js
@@ -0,0 +1,12 @@
+const database = {}
+
+database.messages = [
+    "Hey lyndz, whats up?",
+    "nothing much, doing anything fun this weekend?",
+    "i'm seeing infinity war with some friends",
+    "cool! let me know how it is",
+    "will do!"
+]
+
+localStorage.setItem("messages database", JSON.stringify(database))
+
